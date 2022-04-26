f7a08d3a7dd5433673ae66ecafa94402528c0b81
jean-yves.betton@digital.cabinet-office.gov.uk <jean-yves.betton@digital.cabinet-office.gov.uk>
2022-04-26 17:42:59 +0100

    Modifying Step definition

--- acceptance-tests/src/test/java/uk/gov/di/test/acceptance/AxeStepDefinitions.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/acceptance/AxeStepDefinitions.java
@@ -1,26 +1,15 @@
 package uk.gov.di.test.acceptance;
 
-import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import com.deque.axe.AXE;
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.ACCOUNT_DELETED_CONFIRMATION;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.ACCOUNT_EXISTS;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.CHANGE_PASSWORD;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.DELETE_ACCOUNT;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.ENTER_PASSWORD_CHANGE_PASSWORD;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.ENTER_PASSWORD_DELETE_ACCOUNT;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.MANAGE_YOUR_ACCOUNT;
-import static uk.gov.di.test.acceptance.AccountJourneyPages.PASSWORD_UPDATED_CONFIRMATION;
 import static uk.gov.di.test.acceptance.SignInStepDefinitions.driver;
 
 public class AxeStepDefinitions {
@@ -28,14 +17,14 @@ public class AxeStepDefinitions {
     private static final URL scriptUrl = AxeStepDefinitions.class.getResource("/axe.min.js");
 
     @Given("a user navigates to the build account page")
-    public void a_user_navigates_to_the_build_account_page() throws InterruptedException {
-        driver.get("https://signin.build.account.gov.uk/");
-        Thread.sleep(4000);
+    public void a_user_navigates_to_the_build_account_page() {
+        driver.get("https://build.account.gov.uk");
     }
 
     @When("a user runs accessibility tests")
     public void a_user_runs_accessibility_tests() {
-        System.out.println("Salam2");
+        System.out.println("Page in test = " + driver.getTitle());
+        JSONObject responseJSON = new AXE.Builder(driver.get(), scriptUrl).analyze();
     }
 
     @Then("the user gets results")
--- acceptance-tests/src/test/java/uk/gov/di/test/acceptance/RunCucumberTest.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/acceptance/RunCucumberTest.java
@@ -5,11 +5,5 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-report.html"},
-        features = "src/test/resources/uk.gov.di.test.acceptance",
-        glue = "uk/gov/di/test/acceptance",
-        dryRun = true,
-        tags = "@Axe"
-)
+@CucumberOptions(plugin = {"pretty"})
 public class RunCucumberTest {}
